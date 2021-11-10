3f67b47f63191f2a0a4496c6a93574cde4a0d93f
bosch.richard24 <bosch.richard24@libertycommon.org>
2021-11-10 16:53:24 -0700

    Tele print update 11/04/21

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDrive.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 /**
@@ -33,10 +34,19 @@ public class SimpleDrive extends LinearOpMode {
     private DcMotor right_Back_Drive = null;
     private DcMotor left_Front_Drive = null;
     private DcMotor right_Front_Drive = null;
-    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    // amount to slew servo each CYCLE_MS cycle
     // Define class members
     CRServo   ducky;
-    double  position = 0; // Start at halfway position
+
+    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   50;     // period of each cycle
+    static final double MAX_POS     =  1.0;     // Maximum rotational position
+    static final double MIN_POS     =  0.0;     // Minimum rotational position
+
+    // Define class members
+    Servo   servo;
+    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    boolean rampUp = true;
 
     @Override
     public void runOpMode() {
@@ -81,6 +91,49 @@ public class SimpleDrive extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+
+            // Connect to servo (Assume PushBot Left Hand)
+            // Change the text in quotes to match any servo name on your robot.
+            servo = hardwareMap.get(Servo.class, "hand");
+
+            // Wait for the start button
+            telemetry.addData(">", "Press Start to scan Servo." );
+            telemetry.update();
+            waitForStart();
+
+
+            // Scan servo till stop pressed.
+            while(opModeIsActive()){
+
+                // slew the servo, according to the rampUp (direction) variable.
+                if(gamepad1.x) {
+                    if (rampUp) {
+                        // Keep stepping up until we hit the max value.
+                        position += INCREMENT;
+                        if (position >= MAX_POS) {
+                            position = MAX_POS;
+                            rampUp = !rampUp;   // Switch ramp direction
+                        }
+                    } else {
+                        // Keep stepping down until we hit the min value.
+                        position -= INCREMENT;
+                        if (position <= MIN_POS) {
+                            position = MIN_POS;
+                            rampUp = !rampUp;  // Switch ramp direction
+                        }
+                    }
+                }
+
+                // Display the current value
+                telemetry.addData("Servo Position", "%5.2f", position);
+                telemetry.addData(">", "Press Stop to end test." );
+                telemetry.update();
+
+                // Set the servo to the new position and pause;
+                servo.setPosition(position);
+                sleep(CYCLE_MS);
+                idle();
+            }
 
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
